4014c8a3c837568b8e9cf05c15a86a3aaa9d202d
德芙 <1213159982@qq.com>
2021-10-21 18:07:44 +0800

    添加窗体持久化位置大小拓展

---
+++ Common/SaveFormRectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HDF.Windows.Tools.Common
+{
+    public static class SaveFormRectangleExtension
+    {
+        public static void SaveFormRectangle(this Form form, string filename)
+        {
+            if (string.IsNullOrWhiteSpace(filename))
+                throw new ArgumentException("文件名无效", nameof(filename));
+
+            //记录位置大小
+            var file = Application.StartupPath + "\\" + filename;
+            if (File.Exists(file))
+            {
+                var data = File.ReadAllText(file);
+                var location = data.Split(',').Select(s => Convert.ToInt32(s)).ToArray();
+                form.Location = new System.Drawing.Point(location[0], location[1]);
+            }
+            form.FormClosed += (sender, e) => File.WriteAllText(file, $"{form.Location.X},{form.Location.Y}");
+
+        }
+    }
+}
